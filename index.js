886d53467862af4a46d055c8bb0137e107365b35
Maza455 <same7nte@gmail.com>
2024-02-14 12:07:31 +0200

    Other files

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import path from 'path'
 
 // create an express app
 const app = express()
+const router = express.Router()
+app.use(
+    router,
+    express.static('./static'));
+
 const port = +process.env.PORT || 4000
+
+// Routers: express
+router.get('^/$|/express', display, (req, res)=> {
+    res.status(200).sendFile(path.resolve('./static/html/index.html'))
+})
 
 // Router
 // app.get('/', (req, res)=> {
@@ -11,17 +22,32 @@ const port = +process.env.PORT || 4000
 //     })
 // })
 
-app.get('/', (req, res)=> {
+// app.get('/', display, (req, res)=> {
+//     res.json({
+//         status: res.statusCode,
+//         msg: 'You are getting there!'
+//     })
+// })
+
+// app.get('*', (req, res)=> {
+//     res.json({
+//         status: 404,
+//         msg: '404 page'
+//     })
+// })
+// app.listen(port)
+
+// Middleware
+router.get('^/$|/express', display, (req, res)=> {
     res.json({
         status: res.statusCode,
         msg: 'You are getting there!'
     })
 })
 
-app.get('*', (req, res)=> {
-    res.json({
-        status: 404,
-        msg: '404 page'
-    })
-})
+function display(req, res, next) {
+    console.log("Hello there!");
+    next()
+}
+
 app.listen(port)
